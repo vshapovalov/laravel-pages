6edec6c77fc5df0ed8a224c96a5c33858819731c
Shapovalov Vladimir <shapovalov.v@gmail.com>
2018-04-01 14:42:29 +0600

    fix url pattern

--- routes/Routes.php
+++ routes/Routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::any('/{url?}', ['as' => 'page', 'uses' => '\\Vshapovalov\\Pages\\Http\\Controllers\\PagesController@getPage'])->where(['url' => '^[a-zA-Z0-9_\/.]+$']);
+Route::any('/{url?}', ['as' => 'page', 'uses' => '\\Vshapovalov\\Pages\\Http\\Controllers\\PagesController@getPage'])->where(['url' => '^[a-zA-Z0-9_-\/.]+$']);
